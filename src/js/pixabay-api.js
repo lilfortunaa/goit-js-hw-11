c76c3d93ff0221500962a80ea7bb4f5dad881f13
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-06-14 22:54:55 +0200

    add gallery

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = '50836787-9d290f1c212f5ca84adef07b2';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,73 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-function';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const { elements } = event.target;
+  const request = elements['search-text'].value.trim();
+
+  if (request === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  if (request.length < 3) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Search term must be at least 3 characters long!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  const MIN_LOADER_TIME = 1000;
+  const startTime = Date.now();
+
+  try {
+    const data = await getImagesByQuery(request);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        title: 'No Results',
+        message: 'No images found. Please try again!',
+        position: 'topRight',
+      });
+    }
+
+    const elapsed = Date.now() - startTime;
+    const delay = Math.max(MIN_LOADER_TIME - elapsed, 0);
+
+    setTimeout(() => {
+      hideLoader();
+      if (data.hits.length > 0) {
+        createGallery(data.hits);
+      }
+    }, delay);
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+    });
+    hideLoader();
+  }
+}
